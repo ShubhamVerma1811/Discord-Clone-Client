4fc68e83bee0d5285424a1db0cfd3d718a307d29
Shubham Verma <imshubhamverma.sv@gmail.com>
2021-01-31 17:08:48 +0530

    add: user & server id constraint

--- src/schemas/user_server.sql
+++ src/schemas/user_server.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS user_servers(
   us_uid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   server_uid UUID NOT NULL REFERENCES servers(server_uid) ON DELETE CASCADE,
-  user_uid UUID NOT NULL REFERENCES users(user_uid) ON DELETE CASCADE
+  user_uid UUID NOT NULL REFERENCES users(user_uid) ON DELETE CASCADE,
+  UNIQUE (server_uid, user_uid)
 );
