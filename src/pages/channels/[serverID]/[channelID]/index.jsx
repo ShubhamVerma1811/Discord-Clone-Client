f147447b8b58502337c74e39996836b788b112de
Shubham Verma <imshubhamverma.sv@gmail.com>
2021-01-31 17:09:22 +0530

    cleaned: old code comments

--- src/pages/channels/[serverID]/[channelID]/index.jsx
+++ src/pages/channels/[serverID]/[channelID]/index.jsx
@@ -1,65 +1,34 @@
 // import { useChannel, useEvent, useTrigger } from '@harelpls/use-pusher';
 import Channels from 'components/Channels/';
 import ChannelSideBar from 'components/ChannelSideBar';
+import Main from 'components/Main/Main';
 import Messages from 'components/Messages/Messages';
 import Sidebar from 'components/Sidebar';
 import Layout from 'layouts';
-import { addDashes } from 'services/uuidFormat';
-// import { useEffect, useState, useRef } from 'react';
 
 const channelIDPage = ({ serverID, channelID }) => {
-  // const [msg, setMsg] = useState('');
-  // const messages = useRef({}).current;
-
-  // const channel = useChannel(`presence-send-msg-${channelID}`);
-  // useEvent(channel, 'send-msg', ({ channelID, msg }) => {
-  //   if (!messages[channelID]) {
-  //     messages[channelID] = [];
-  //   }
-  //   messages[channelID].push(msg);
-  // });
-  // const trigger = useTrigger(`presence-send-msg-${channelID}`);
-
-  // console.log(messages);
-  // useEffect(() => {}, [messages]);
-
-  // y.slice(0,8)+"-"+y.slice(8,12)+"-"+y.slice(12,16)+"-"+y.slice(16,20)+"-"+y.slice(20)
-
   return (
     <div>
       <Layout>
         <Sidebar>
           <Sidebar.ChannelSideBar>
             <ChannelSideBar>
-              <Channels serverID={serverID} channelID={addDashes(channelID)} />
+              <Channels serverID={serverID} channelID={channelID} />
             </ChannelSideBar>
           </Sidebar.ChannelSideBar>
         </Sidebar>
         <div className="bg-gray-600 w-full">
-          <h1>MAIN CONTENT</h1>
-          <h1>Messages for channelID : {channelID}</h1>
-          <Messages channelID={channelID} />
-          {/* {messages[channelID]?.map((msg, index) => (
-            <h1 key={index}>{msg}</h1>
-          ))}
-          <form
-            onSubmit={async (e) => {
-              e.preventDefault();
-              trigger('send-msg', {
-                channelID,
-                msg,
-              });
-              setMsg('');
-            }}>
-            <input
-              type="text"
-              value={msg}
-              placeholder="ENTER MSG"
-              onChange={(e) => setMsg(e.target.value)}
-            />
-            <button type="submit">Submit</button>
-          </form> */}
+          <Main>
+            <h1>MAIN CONTENT</h1>
+            <h1>Messages for channelID : {channelID}</h1>
+            <Messages channelID={channelID} />
+          </Main>
         </div>
+        <Sidebar>
+          <ChannelSideBar>
+            <Channels serverID={serverID} channelID={channelID} />
+          </ChannelSideBar>
+        </Sidebar>
       </Layout>
     </div>
   );
