2ab14c749058068ee8d88a97521d2d0cc80d4e4b
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-02-27 16:10:14 +0530

    sorted msgs in asc

--- src/pages/api/channels/[cid]/messages.js
+++ src/pages/api/channels/[cid]/messages.js
@@ -3,7 +3,7 @@ import pool from 'lib/db';
 export default async function getChannelMessages(req, res) {
   const { cid } = req.query;
   const query =
-    'SELECT * FROM messages INNER JOIN users ON users.user_uid=messages.author_uid WHERE channel_uid=$1 ORDER BY messages.created_at DESC';
+    'SELECT * FROM messages INNER JOIN users ON users.user_uid=messages.author_uid WHERE channel_uid=$1 ORDER BY messages.created_at';
   const values = [cid];
   const result = await pool.query(query, values);
 
