31287a1eb123b264beb8cbec3f4234772cea3297
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-03-21 16:23:46 +0530

    better CSS

--- src/components/ServerSideBar/ServerSideBar.jsx
+++ src/components/ServerSideBar/ServerSideBar.jsx
@@ -45,21 +45,31 @@ export const ServerSideBar = () => {
 
   const createServerUI = () => {
     return (
-      <div>
+      <div className="h-full">
         <form onSubmit={handleCreateServer}>
-          <input
-            type="text"
-            placeholder="name"
-            required
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="description"
-            required
-            onChange={(e) => setDescription(e.target.value)}
-          />
-          <button type="submit">Create</button>
+          <div>
+            <div className="flex justify-center items-center">
+              <input
+                className="p-3"
+                type="text"
+                placeholder="name"
+                required
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                className="p-3"
+                type="text"
+                placeholder="description"
+                required
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <div>
+              <button className="bg-blue-600 px-3 py-2" type="submit">
+                Create
+              </button>
+            </div>
+          </div>
         </form>
       </div>
     );
@@ -67,7 +77,7 @@ export const ServerSideBar = () => {
 
   const joinServerUI = () => {
     return (
-      <div>
+      <div className="h-full">
         <form
           onSubmit={async (e) => {
             e.preventDefault();
@@ -126,7 +136,7 @@ export const ServerSideBar = () => {
                   key={server.server_uid}>
                   <div
                     className={`logo rounded-full my-2 ${
-                      serverID == server.server_uid
+                      serverID == server.server_uid.split('-').join('')
                         ? 'bg-blue-500 logo-selected'
                         : 'bg-gray-700 text-white'
                     } h-16 w-16 flex font-bold items-center justify-center cursor-pointer hover:bg-blue-500`}>
@@ -153,10 +163,10 @@ export const ServerSideBar = () => {
       </div>
       <div>
         <Modal open={serverModal} onClose={() => setServerModal(false)} center>
-          <div className="bg-gray-700">
+          <div className="bg-gray-700 flex justify-center">
             <div className="flex">
-              {createServerUI()}
-              {joinServerUI()}
+              <div className="px-1">{createServerUI()}</div>
+              <div className="px-1">{joinServerUI()}</div>
             </div>
           </div>
         </Modal>
