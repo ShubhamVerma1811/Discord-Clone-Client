ac23be6b767f96f95cf90b0a2c8d14d082efa3a1
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-03-21 16:20:47 +0530

    moved presence channel creation

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,34 +1,50 @@
+import { useEvent, useTrigger } from '@harelpls/use-pusher';
 import Avatar from 'components/Avatar/Avatar';
 import moment from 'moment';
-import { useState } from 'react';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useEffect, useState } from 'react';
+import { useMutation, useQuery } from 'react-query';
 import { useSelector } from 'react-redux';
 import { getChannelMessages } from 'services/handleChannelsData';
 import { createMessage } from 'services/handleMessagesData';
 
-const Messages = ({ channelID }) => {
+const Messages = ({ channelID, presenceChannel }) => {
   const [msg, setMsg] = useState('');
+  const [msgs, setMsgs] = useState([]);
   const user = useSelector((state) => state.user);
-  const queryClient = useQueryClient();
-  const mutation = useMutation(async (newMsg) => {
-    const res = await createMessage(newMsg);
-    queryClient.invalidateQueries('getChannelMessages');
-  });
 
   const { data, isLoading } = useQuery(
     ['getChannelMessages', { channelID }],
     () => getChannelMessages(channelID)
   );
 
+  const mutation = useMutation(async (newMsg) => {
+    await createMessage(newMsg);
+  });
+
+  useEvent(presenceChannel, 'send-msg', (msgData) => {
+    setMsgs((prev) => [...prev, { ...msgData, username: msgData.authorName }]);
+  });
+  const trigger = useTrigger(presenceChannel?.name);
+
+  useEffect(() => {
+    setMsgs([]);
+  }, [channelID]);
+
+  useEffect(() => {
+    if (data?.messages) setMsgs((prev) => [...prev, ...data?.messages]);
+  }, [channelID, isLoading]);
+
   const handleSendMessage = async (e) => {
     e.preventDefault();
-    mutation.mutate({
+    const msgData = {
       context: msg,
       channelID,
       authorID: user.user_uid,
       createdAt: Date.now(),
       updatedAt: Date.now(),
-    });
+    };
+    mutation.mutate(msgData);
+    trigger('send-msg', { ...msgData, authorName: user.username });
     setMsg('');
   };
 
@@ -37,35 +53,39 @@ const Messages = ({ channelID }) => {
       {isLoading ? (
         <h1>Loading...</h1>
       ) : (
-        data.messages.map((message) => (
-          <div key={message.message_id}>
-            <div className="flex">
-              <div className="w-12 h-12 overflow-hidden rounded-full">
-                <Avatar
-                  src="https://firebasestorage.googleapis.com/v0/b/tweeter-45929.appspot.com/o/_MG_9752.JPG?alt=media&token=84363dfc-4111-4c5e-b569-0852da768639"
-                  alt="profile_picture"
-                />
-              </div>
-              <div>
-                <div className="flex">
-                  <div className="p-2">
-                    <p>{message.username}</p>
-                  </div>
-                  <div className="p-2">
-                    <p>{moment().calendar()}</p>
-                  </div>
+        <div>
+          {msgs.map((message) => (
+            <div key={message.message_uid}>
+              <div className="flex">
+                <div className="w-12 h-12 overflow-hidden rounded-full">
+                  <Avatar
+                    src="https://firebasestorage.googleapis.com/v0/b/tweeter-45929.appspot.com/o/_MG_9752.JPG?alt=media&token=84363dfc-4111-4c5e-b569-0852da768639"
+                    alt="profile_picture"
+                  />
                 </div>
-                <div>{message.context}</div>
+                <div>
+                  <div className="flex">
+                    <div className="p-2">
+                      <p>{message.username}</p>
+                    </div>
+                    <div className="p-2">
+                      <p>{moment(message.created_at).format('lll')}</p>
+                    </div>
+                  </div>
+                  <div>{message.context}</div>
+                </div>
               </div>
             </div>
-          </div>
-        ))
+          ))}
+        </div>
       )}
+
       <form onSubmit={handleSendMessage}>
         <input
           type="text"
           value={msg}
           placeholder="Send Message"
+          required
           onChange={(e) => setMsg(e.target.value)}
         />
         <button type="submit">Submit</button>
