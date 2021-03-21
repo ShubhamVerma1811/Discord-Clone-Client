0bb94f8dbca7df9dc764d68682b6ac3b486ddcbb
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-03-21 16:22:47 +0530

    moved presence channel to page

--- src/pages/channels/[serverID]/[channelID]/index.jsx
+++ src/pages/channels/[serverID]/[channelID]/index.jsx
@@ -1,13 +1,17 @@
-// import { useChannel, useEvent, useTrigger } from '@harelpls/use-pusher';
+import { usePresenceChannel } from '@harelpls/use-pusher';
 import Channels from 'components/Channels/';
 import ChannelSideBar from 'components/ChannelSideBar';
 import Main from 'components/Main/Main';
 import Messages from 'components/Messages/Messages';
+import PeopleSideBar from 'components/PeopleSideBar/PeopleSideBar';
 import Sidebar from 'components/Sidebar';
 import withAuth from 'hoc/withAuth';
 import Layout from 'layouts';
 
 const channelIDPage = ({ serverID, channelID }) => {
+  const channelName = `presence-channel-${channelID}`;
+  const { channel, members, me, myID, count } = usePresenceChannel(channelName);
+
   return (
     <div>
       <Layout>
@@ -22,9 +26,13 @@ const channelIDPage = ({ serverID, channelID }) => {
           <Main>
             <h1>MAIN CONTENT</h1>
             <h1>Messages for channelID : {channelID}</h1>
-            <Messages channelID={channelID} />
+            <Messages channelID={channelID} presenceChannel={channel} />
           </Main>
         </div>
+        <PeopleSideBar
+          members={members}
+          serverID={serverID}
+          channelID={channelID}></PeopleSideBar>
       </Layout>
     </div>
   );
